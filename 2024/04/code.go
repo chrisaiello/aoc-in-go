79c131a5cf5e62ef6e84fd70e9d3cea2166bef24
Chris Aiello <chris.aiello@andgosystems.com>
2024-12-04 10:16:17 -0600

    comment day3

--- 2024/04/code.go
+++ 2024/04/code.go
@@ -8,10 +8,12 @@ import (
 )
 
 func main() {
-	aoc.Harness(run)
+	aoc.Harness(run1)
+	aoc.Harness(run2)
 }
 
-func run(part2 bool, input string) any {
+// Part 2
+func run2(part2 bool, input string) any {
 	MAS := []string{"M", "A", "S"}
 	lines := strings.Split(input, "\n")
 	grid := make([][]string, len(lines))
@@ -40,41 +42,38 @@ func run(part2 bool, input string) any {
 					x_new := x + dir[0]
 					y_new := y + dir[1]
 					var key string
-					for z := 1; z < 3; z++ {
-						//fmt.Println("x:", x, "y:", y, "z:", z)
-						//fmt.Println("Looking for:", MAS[z], "in", dir)
+					for z := 1; z < len(MAS); z++ {
 
 						if x_new < 0 || y_new < 0 {
-							//fmt.Println("BRK < 0")
-							//count++
+							// We're outside of the grid
 							break
 						}
 
 						if x_new > len(grid)-1 || y_new > len(grid)-1 {
-							//fmt.Println("BRK out of grid")
-							//count++
+							// We're outside of the grid
 							break
 						}
 
 						if grid[x_new][y_new] != MAS[z] {
-							//fmt.Println("BRK wrong letter")
-							//count++
+							// We didn't find the right letter
 							break
 						}
 						if MAS[z] == "A" && grid[x_new][y_new] == "A" {
+							// We found the middle "A" in the right spot
+							// Record its index so we can find overlapping MAS's
 							key = fmt.Sprintf("%d,%d", x_new, y_new)
 						}
-						// If we found the "A" in "MAS",
-						// record its coordinates
-						// Later, check for multiple ocurrences of the
-						// same "A" coordinate
 
-						if z == 2 {
+						if z == len(MAS)-1 {
+							// We reached the end of "MAS"
 							fmt.Println("FOUND MAS")
+							// Save the coordinate of the "A"
 							m[key] += 1
 							count++
+							break
 						}
 
+						// Continue searching in the same direction
 						x_new += dir[0]
 						y_new += dir[1]
 
@@ -85,11 +84,10 @@ func run(part2 bool, input string) any {
 		}
 	}
 
-	//fmt.Printf("%T", grid)
-	//fmt.Println(grid[9][19])
 	fmt.Println(m)
 	x_count := 0
 	for key, value := range m {
+		// Count instances where the same "A" coordinate is shared between multiple words
 		if value == 2 {
 			fmt.Println(key, value)
 			x_count++
@@ -98,72 +96,69 @@ func run(part2 bool, input string) any {
 	return x_count
 }
 
-// func run(part2 bool, input string) any {
-// 	XMAS := []string{"X", "M", "A", "S"}
-// 	lines := strings.Split(input, "\n")
-// 	grid := make([][]string, len(lines))
-// 	for i := range grid {
-// 		grid[i] = strings.Split(lines[i], "")
-// 	}
+// Part 1
+func run1(part2 bool, input string) any {
+	XMAS := []string{"X", "M", "A", "S"}
+	lines := strings.Split(input, "\n")
+	grid := make([][]string, len(lines))
+	for i := range grid {
+		grid[i] = strings.Split(lines[i], "")
+	}
 
-// 	dirs := [][]int{
-// 		// {x,y} search directions
-// 		{-1, -1},
-// 		{-1, 0},
-// 		{-1, 1},
-// 		{0, -1},
-// 		{0, 1},
-// 		{1, -1},
-// 		{1, 0},
-// 		{1, 1},
-// 	}
-// 	count := 0
-// 	for x := range grid {
-// 		for y := range grid {
-// 			//count++
-// 			if grid[x][y] == XMAS[0] {
-// 				//count++
-// 				for _, dir := range dirs {
-// 					x_new := x + dir[0]
-// 					y_new := y + dir[1]
-// 					for z := 1; z < 4; z++ {
-// 						fmt.Println("x:", x, "y:", y, "z:", z)
-// 						fmt.Println("Looking for:", XMAS[z], "in", dir)
+	dirs := [][]int{
+		// {x,y} search directions
+		{-1, -1},
+		{-1, 0},
+		{-1, 1},
+		{0, -1},
+		{0, 1},
+		{1, -1},
+		{1, 0},
+		{1, 1},
+	}
+	count := 0
+	for x := range grid {
+		for y := range grid {
+			if grid[x][y] == XMAS[0] {
+				// Our initial check, is the current letter "X"?
+				for _, dir := range dirs {
+					// Search in every direction
+					x_new := x + dir[0]
+					y_new := y + dir[1]
+					for z := 1; z < len(XMAS); z++ {
+						fmt.Println("x:", x, "y:", y, "z:", z)
+						fmt.Println("Looking for:", XMAS[z], "in", dir)
 
-// 						if x_new < 0 || y_new < 0 {
-// 							fmt.Println("BRK < 0")
-// 							//count++
-// 							break
-// 						}
+						if x_new < 0 || y_new < 0 {
+							// We're outside of the grid
+							break
+						}
 
-// 						if x_new > len(grid)-1 || y_new > len(grid)-1 {
-// 							fmt.Println("BRK out of grid")
-// 							//count++
-// 							break
-// 						}
+						if x_new > len(grid)-1 || y_new > len(grid)-1 {
+							// We're outside of the grid
+							break
+						}
 
-// 						if grid[x_new][y_new] != XMAS[z] {
-// 							fmt.Println("BRK wrong letter")
+						if grid[x_new][y_new] != XMAS[z] {
+							// We didn't find the right letter
+							break
+						}
+						if z == len(XMAS)-1 {
+							// We reached the end of XMAS
+							fmt.Println("FOUND XMAS")
+							count++
+							break
+						}
+						// Continue searching in the same direction
+						x_new += dir[0]
+						y_new += dir[1]
 
-// 							//count++
-// 							break
-// 						}
-// 						if z == 3 {
-// 							fmt.Println("FOUND XMAS")
-// 							count++
-// 						}
+					}
+				}
 
-// 						x_new += dir[0]
-// 						y_new += dir[1]
+			}
+		}
+	}
 
-// 					}
-// 				}
-
-// 			}
-// 		}
-// 	}
-
-// 	//fmt.Printf("%T", grid)
-// 	//fmt.Println(grid[9][19])
-// 	return count
-// }
+	return count
+}

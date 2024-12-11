e98e2f19386ebfea7e7835c43eb5b0fa7ddd84e6
Chris Aiello <chris.aiello@andgosystems.com>
2024-12-11 08:44:52 -0600

    update

--- 2024/05/code.go
+++ 2024/05/code.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"slices"
 	"strconv"
 	"strings"
 
@@ -9,7 +10,90 @@ import (
 )
 
 func main() {
-	aoc.Harness(run)
+	aoc.Harness(run1)
+}
+
+var page_number_map map[int]int
+
+func validate_book(book []int) (bool, int) {
+	nbook := make([]int, len(book))
+	copy(nbook, book)
+	mapped_book := map_book(nbook)
+	if ascending(mapped_book) {
+		return true, book[len(book)/2]
+	} else {
+		return false, 0
+	}
+}
+
+func map_book(book []int) []int {
+	for i, page := range book {
+		mapped_page := page_number_map[page]
+		book[i] = mapped_page
+	}
+	return book
+}
+
+func contains(candidate int, rules [][]int) bool {
+	for _, rule := range rules {
+		if candidate == rule[1] {
+			fmt.Println("Candidate", candidate, "was found in", rule)
+			return false
+		}
+	}
+	fmt.Println("This must be the lowest")
+	return true
+}
+
+func prune(x int, rules [][]int) [][]int {
+	rules = slices.DeleteFunc(rules, func(n []int) bool {
+		return n[0] == x
+	})
+	return rules
+}
+
+func gen_page_number_map(rules [][]int) any {
+	fmt.Println("starting")
+	fmt.Println(len(rules))
+	if len(rules) == 1 {
+		fmt.Println("Reached last element")
+		page_number_map[rules[0][0]] = len(page_number_map)
+		page_number_map[rules[0][1]] = len(page_number_map)
+		return true
+	}
+
+	for _, rule := range rules {
+
+		if len(rules) == 0 || rule == nil {
+			fmt.Println("Reached end?")
+			return true
+		}
+		candidate := rule[0]
+		//fmt.Println("Candidate:", candidate)
+		is_lowest := contains(candidate, rules)
+		if is_lowest {
+			page_number_map[candidate] = len(page_number_map)
+			//fmt.Println("Rules:", rules, "Removing:", candidate)
+			rules = prune(candidate, rules)
+			//fmt.Println("New rules:", rules)
+			gen_page_number_map(rules)
+		}
+
+	}
+
+	return true
+}
+
+func ascending(s []int) bool {
+	for i := 0; i < len(s); i++ {
+		if i == len(s)-1 {
+			return true
+		}
+		if s[i] >= s[i+1] {
+			return false
+		}
+	}
+	return false
 }
 
 func run(part2 bool, input string) any {
@@ -35,7 +119,103 @@ func run(part2 bool, input string) any {
 		}
 		books[i] = s2
 	}
-	fmt.Println("Rules:\n", rules, "\n...")
-	fmt.Println("Books:\n", books)
-	return 1
+	page_number_map = make(map[int]int)
+	fmt.Println("Taking", rules[0][0], "as starting point")
+	page_number_map[rules[0][0]] = len(page_number_map)
+	rules = prune(rules[0][0], rules)
+	gen_page_number_map(rules)
+	//fmt.Println(ascending([]int{1, 5, 6, 5}))
+	fmt.Println(page_number_map)
+	sum := 0
+	for _, book := range books {
+		_, mid := validate_book(book)
+		sum += mid
+		//fmt.Println(valid, mid)
+	}
+	//fmt.Println("Rules:\n", rules, "\n...")
+	//fmt.Println("Books:\n", books)
+	return sum
+}
+func rules_contain1(rules [][]int, test_rule []int) bool {
+	for _, rule := range rules {
+		if rule[0] == test_rule[0] && rule[1] == test_rule[1] {
+			return true
+		}
+	}
+	return false
+}
+
+// Part 1
+func validate_book1(book []int, rules [][]int) (bool, int) {
+	for i, outer_page := range book {
+		for _, check_page := range book[i+1:] {
+			if rules_contain1(rules, []int{check_page, outer_page}) {
+				return false, 0
+			}
+		}
+	}
+	return true, book[len(book)/2]
+}
+
+func get_sorted_middle2(book []int, rules [][]int) int {
+	len := len(book)
+	matches := 0
+	fmt.Println(book)
+	for _, outer_page := range book {
+		for _, check_page := range book {
+			if rules_contain1(rules, []int{check_page, outer_page}) {
+				matches++
+			}
+		}
+		if matches == len/2 {
+			return outer_page
+		}
+		matches = 0
+	}
+	return 0
+}
+
+// Part 2
+func validate_book2(book []int, rules [][]int) (bool, int) {
+	for i, outer_page := range book {
+		for _, check_page := range book[i+1:] {
+			if rules_contain1(rules, []int{check_page, outer_page}) {
+				return false, get_sorted_middle2(book, rules)
+			}
+		}
+	}
+	return true, 0
+}
+
+func run1(part2 bool, input string) any {
+	parts := strings.Split(input, "\n\n")
+	rules_s := strings.Split(parts[0], "\n")
+	books_s := strings.Split(parts[1], "\n")
+
+	rules := make([][]int, len(rules_s))
+	for i, rule := range rules_s {
+		s1 := strings.Split(rule, "|")
+		x1, _ := strconv.Atoi(s1[0])
+		x2, _ := strconv.Atoi(s1[1])
+		rules[i] = []int{x1, x2}
+	}
+
+	books := make([][]int, len(books_s))
+	for i, book := range books_s {
+		s1 := strings.Split(book, ",")
+		s2 := make([]int, len(s1))
+		for j, s := range s1 {
+			x, _ := strconv.Atoi(s)
+			s2[j] = x
+		}
+		books[i] = s2
+	}
+	sum := 0
+	for _, book := range books {
+		valid, mid := validate_book2(book, rules)
+		sum += mid
+		fmt.Println(valid, mid)
+	}
+
+	return sum
 }
--- 2024/09/code.go
+++ 2024/09/code.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"slices"
+	"strconv"
+	"strings"
+
 	"github.com/jpillora/puzzler/harness/aoc"
 )
 
@@ -8,7 +13,162 @@ func main() {
 	aoc.Harness(run)
 }
 
-func run(part2 bool, input string) any {
+func check_ordered1(layout []string) bool {
+	dot_index := slices.Index(layout, ".")
 
-	return 42
+	for i := dot_index; i < len(layout); i++ {
+		if layout[i] != "." {
+			//fmt.Println("Not sorted yet")
+			return false
+		}
+	}
+	fmt.Println("We're sorted!")
+	return true
+}
+
+func move1(layout []string) ([]string, bool) {
+
+	if check_ordered1(layout) {
+		return layout, true
+	}
+
+	id_to_move := -1
+	//fmt.Println(len(layout))
+	for i := len(layout) - 1; i >= 0; i-- {
+		//fmt.Println("Checking", layout[i], "at", i)
+		if layout[i] != "." {
+			//fmt.Println("Moving", layout[i], "at", i)
+			id_to_move = i
+			break
+		}
+	}
+
+	for i, el := range layout {
+		if el == "." {
+			layout[i] = layout[id_to_move]
+			layout[id_to_move] = "."
+			break
+		}
+	}
+	return layout, false
+}
+
+func score(layout []string) int {
+	sum := 0
+	for i, el := range layout {
+		el_int := 0
+		if el != "." {
+			el_int, _ = strconv.Atoi(el)
+		}
+		sum += el_int * i
+	}
+	return sum
+}
+
+func has_free_space(layout []string, file_size int, candidate_index int) (bool, int) {
+	candidate := -1
+	size := 0
+
+	for i, el := range layout {
+		if i == candidate_index {
+			return false, -1
+		}
+		if el == "." {
+			candidate = i
+			size++
+			for j := i + 1; j < len(layout); j++ {
+				if layout[j] == "." {
+					//fmt.Println("size", size, "++ing")
+					size++
+				} else {
+					break
+				}
+			}
+			if size >= file_size {
+				//fmt.Println(layout)
+				fmt.Println("Layout has free space size", size, "at index", candidate)
+				return true, candidate
+			}
+		}
+		size = 0
+	}
+	return false, -1
+}
+
+func move2(layout []string, char string) []string {
+
+	fmt.Println("Moving", char)
+	index := slices.Index(layout, char)
+	file_size := 0
+	for i := index; i <= len(layout); i++ {
+		if i == len(layout) || layout[i] != char {
+			break
+		}
+		file_size++
+	}
+	fmt.Println(char, "Begins at index", index, "size", file_size)
+
+	has_free_space, free_space_index := has_free_space(layout, file_size, index)
+
+	//fmt.Println(has_free_space, free_space_index)
+
+	if !has_free_space {
+		fmt.Println("No free space")
+		return layout
+	}
+
+	for i := 0; i < file_size; i++ {
+		//fmt.Println("Setting layout index", i, "to", char)
+		layout[i+free_space_index] = char
+		layout[i+index] = "."
+	}
+	//fmt.Println(layout)
+
+	return layout
+}
+
+func run(part2 bool, input string) any {
+	in := make([]int, 0)
+	in_sum := 0
+	for _, char := range strings.Split(input, "") {
+		num, _ := strconv.Atoi(char)
+		in = append(in, num)
+		in_sum += num
+	}
+	//fmt.Println(in_sum)
+
+	layout := make([]string, 0)
+	//fmt.Println(len(layout))
+
+	j := 0
+	for i, char := range in {
+		write_char := strconv.Itoa(j)
+		if i%2 == 1 {
+			write_char = "."
+		} else {
+			j++
+		}
+		for i := 0; i < char; i++ {
+			layout = append(layout, write_char)
+		}
+	}
+	//fmt.Println(layout)
+
+	// we now have a []string, layout, with the disk layout
+	// i.e. 0..111....22222
+	// Input always seems to be odd, which means the layout always starts and ends with
+	// a file, not empty space
+
+	// done := false
+	// for i := 0; done == false; i++ {
+	// 	layout, done = move1(layout)
+	// }
+	for i := len(in) / 2; i >= 0; i-- {
+		i_s := strconv.Itoa(i)
+		//fmt.Println(layout)
+		layout = move2(layout, i_s)
+		//fmt.Println(layout)
+	}
+
+	return score(layout)
 }
